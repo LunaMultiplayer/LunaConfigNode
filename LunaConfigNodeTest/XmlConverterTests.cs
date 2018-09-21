19dcd7091a09a56485e6e0e4d71e2de793a5f473
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-09-21 11:19:34 +0200

    Added a xml converter and it's tests

---
+++ LunaConfigNodeTest/XmlConverterTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using LunaConfigNode;
+using LunaConfigNodeTest.Extension;
+
+namespace LunaConfigNodeTest
+{
+    [TestClass]
+    public class XmlConverterTests
+    {
+        private static readonly string XmlExamplePath = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "Examples", "Others");
+
+        [TestMethod]
+        public void TestSeveralVesselsToXml()
+        {
+            foreach (var file in Directory.GetFiles(Path.Combine(XmlExamplePath)))
+            {
+                SwitchToXmlAndBack(file);
+            }
+        }
+
+        private static void SwitchToXmlAndBack(string filePath)
+        {
+            if (!File.Exists(filePath)) return;
+
+            var configNode = File.ReadAllText(filePath).ToUnixString();
+            var xml = XmlConverter.ConvertToXml(configNode);
+            var backToConfigNode = XmlConverter.ConvertToConfigNode(xml).ToUnixString();
+
+            Assert.IsTrue(configNode.Equals(backToConfigNode), $"Error serializing config node. File: {Path.GetFileName(filePath)}");
+        }
+    }
+}
